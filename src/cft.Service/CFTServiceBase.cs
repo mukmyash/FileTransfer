eee0642b366e324c7b39cc2c6e8c75519f245f26
mukmyash <mukminovartur@gmail.com>
2019-06-23 23:49:25 +0500

    Досохраняю

--- src/cft.Service/CFTServiceBase.cs
+++ src/cft.Service/CFTServiceBase.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.ServiceProcess;
@@ -9,10 +10,55 @@ namespace cft.Service
     internal class CFTServiceBase : ServiceBase
     {
         private readonly IHost _host;
+        private bool _stopRequestedByWindows;
 
         public CFTServiceBase(IHost host)
         {
             _host = host ?? throw new ArgumentNullException(nameof(host));
         }
+
+        protected sealed override void OnStart(string[] args)
+        {
+            OnStarting(args);
+
+            _host
+                .Services
+                .GetRequiredService<IApplicationLifetime>()
+                .ApplicationStopped
+                .Register(() =>
+                {
+                    if (!_stopRequestedByWindows)
+                    {
+                        Stop();
+                    }
+                });
+
+            _host.Start();
+
+            OnStarted();
+        }
+
+        protected sealed override void OnStop()
+        {
+            _stopRequestedByWindows = true;
+            OnStopping();
+            try
+            {
+                _host.StopAsync().GetAwaiter().GetResult();
+            }
+            finally
+            {
+                _host.Dispose();
+                OnStopped();
+            }
+        }
+
+        protected virtual void OnStarting(string[] args) { }
+
+        protected virtual void OnStarted() { }
+
+        protected virtual void OnStopping() { }
+
+        protected virtual void OnStopped() { }
     }
 }
